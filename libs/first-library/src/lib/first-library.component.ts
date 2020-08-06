587804c60f6e8d370f93b5e43995dcc4194ee942
Sergey Alemasov <s.alemasov@tinkoff.ru>
2020-08-06 16:01:59 +0400

    fix: fixes for lints

---
+++ libs/first-library/src/lib/first-library.component.ts
@@ -0,0 +1,9 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'first-library',
+  template: '',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class FirstLibraryComponent {
+}
